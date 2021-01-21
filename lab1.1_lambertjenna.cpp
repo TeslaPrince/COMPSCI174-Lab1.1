0413514f39b5fa28020beeeaff2596a5e0323365
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 00:44:01 -0600

    Add files via upload

---
+++ lab1.1_lambertjenna.cpp
@@ -0,0 +1,16 @@
+//Jenna Lambert
+//2-13-19
+//Initials
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "JJJJJJJJJJ   CCCCCC  TTTTTTTTT L" << endl;
+	cout << "     J      C      C     T     L" << endl;
+	cout << "     J     C             T     L" << endl;
+	cout << "     J     C             T     L" << endl;
+	cout << " J   J      C      C     T     L" << endl;
+	cout << "  JJJ        CCCCCC      T     LLLLLL" << endl;
+}
